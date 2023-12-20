4595241683f7b21ebf75957e5402f1ac57b9a6da
chern <chernenkij.nikitych@gmail.com>
2023-12-20 14:32:37 +0100

    Курс 4 Домашнее задание 41 Телеграм бот командная работа - изначальный комментарий

---
+++ src/main/java/skypro/teamworktelegrambot/Kurs4DomashneeZadanie41KomandnayaRabotaTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package skypro.teamworktelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kurs4DomashneeZadanie41KomandnayaRabotaTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kurs4DomashneeZadanie41KomandnayaRabotaTelegramBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/skypro/teamworktelegrambot/Kurs4DomashneeZadanie41KomandnayaRabotaTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package skypro.teamworktelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kurs4DomashneeZadanie41KomandnayaRabotaTelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
